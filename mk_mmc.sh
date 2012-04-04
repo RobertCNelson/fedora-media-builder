a228f017d6f96e043f465bacf50e1f4a8c12ee3e
Robert Nelson <robertcnelson@gmail.com>
2012-04-04 09:05:22 -0500

    board: igepv2 use bootz by default
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -426,15 +426,7 @@ function boot_uenv_txt_template {
 
 		__EOF__
 		;;
-	igepv2)
-		cat >> ${TEMPDIR}/bootscripts/normal.cmd <<-__EOF__
-			optargs=VIDEO_CONSOLE
-			deviceargs=setenv device_args
-			loaduimage=run xyz_mmcboot; run deviceargs; run mmcargs; \${boot} \${address_image}
-
-		__EOF__
-		;;
-	crane|mx51evk|mx53loco|panda|panda_es)
+	crane|igepv2|mx51evk|mx53loco|panda|panda_es)
 		cat >> ${TEMPDIR}/bootscripts/normal.cmd <<-__EOF__
 			optargs=VIDEO_CONSOLE
 			deviceargs=setenv device_args
@@ -1118,6 +1110,7 @@ function check_uboot_type {
 		BOOTLOADER="IGEP00X0"
 		SERIAL="ttyO2"
 		is_omap
+		USE_ZIMAGE=1
 
 		SERIAL_MODE=1
 		;;
