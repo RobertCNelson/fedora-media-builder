9e8940800da26f16f4c31dce7dbbfa510532f6c5
Robert Nelson <robertcnelson@gmail.com>
2012-06-19 13:16:18 -0500

    script: rename ZRELADD to load_addr
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mk_mmc.sh
+++ mk_mmc.sh
@@ -704,7 +704,7 @@ function populate_boot {
 			LINUX_VER=$(ls ${TEMPDIR}/kernel/boot/${VMLINUZ} | awk -F'vmlinuz-' '{print $2}')
 			if [ ! "${USE_ZIMAGE}" ] ; then
 				echo "Using mkimage to create uImage"
-				mkimage -A arm -O linux -T kernel -C none -a ${ZRELADD} -e ${ZRELADD} -n ${LINUX_VER} -d ${TEMPDIR}/kernel/boot/${VMLINUZ} ${TEMPDIR}/disk/uImage
+				mkimage -A arm -O linux -T kernel -C none -a ${load_addr} -e ${load_addr} -n ${LINUX_VER} -d ${TEMPDIR}/kernel/boot/${VMLINUZ} ${TEMPDIR}/disk/uImage
 				echo "-----------------------------"
 			fi
 			echo "Copying Kernel image:"
@@ -727,6 +727,7 @@ function populate_boot {
 			board=${BOOTLOADER}
 			kernel_addr=${kernel_addr}
 			initrd_addr=${initrd_addr}
+			load_addr=${load_addr}
 
 		__EOF__
 
@@ -1001,8 +1002,7 @@ function is_omap {
 
 	kernel_addr="0x80300000"
 	initrd_addr="0x81600000"
-
-	ZRELADD="0x80008000"
+	load_addr="0x80008000"
 
 	SERIAL_CONSOLE="${SERIAL},115200n8"
 
@@ -1176,9 +1176,9 @@ function check_uboot_type {
 		SERIAL="ttymxc0"
 		is_imx
 		USE_ZIMAGE=1
-		ZRELADD="0x90008000"
 		kernel_addr="0x90800000"
 		initrd_addr="0x92100000"
+		load_addr="0x90008000"
 		BETA_KERNEL=1
 		SERIAL_MODE=1
 		;;
@@ -1190,9 +1190,9 @@ function check_uboot_type {
 		SERIAL="ttymxc0"
 		is_imx
 		USE_ZIMAGE=1
-		ZRELADD="0x70008000"
 		kernel_addr="0x70800000"
 		initrd_addr="0x72100000"
+		load_addr="0x70008000"
 		BETA_KERNEL=1
 		SERIAL_MODE=1
 		;;
